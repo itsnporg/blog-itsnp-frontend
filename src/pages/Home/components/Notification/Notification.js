523c5556f27d88076b7df2f4fa8d62e14972f29c
chetan-budathoki <chetanbudhathoki6@gmail.com>
2021-06-26 16:37:09 +0545

    navbar component made -notification component made -shame: couldn't use mixins and fonts

--- src/pages/Home/components/Notification/Notification.js
+++ src/pages/Home/components/Notification/Notification.js
@@ -1,96 +1,87 @@
 import React from "react";
 import "./Notification.scss";
-import {ReactComponent as NotificationBellIcon} from "../../assets/icons/notificationbell.svg";
+import { ReactComponent as NotificationBellIcon } from "../../assets/icons/notificationbell.svg";
+import {NotificationData} from "./NotificationData";
 
-export const Notification=()=> {
-    return (
-
-       <div>
+export const Notification = () => {
+  return (
+    <div>
       <div className="notification_container">
         <div className="arrow_head_container">
-         <div className="arrow_head">
-  
-         </div>
+          <div className="arrow_head"></div>
         </div>
         <div className="notification_titles">
           <p>Notification</p>
           <div className="icon">
             <NotificationBellIcon className="icon" />
+          </div>
+        </div>
+        {NotificationData.map((item) => {
+          return (
+            <div className="notifications">
+              <div className="profile">
+                <img
+                  src={item.profileimg}
+                  alt=""
+                />
+              </div>
+              <div className="notification_value">
+               {item.notification}
+              </div>
+            </div>
+          );
+        })}
 
-          </div>
-        </div>
-        <div className="notifications">
-          <div className="profile">
-            
-              <img src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg" alt=""/>
-            
-          </div>
-          <div className="notification_value">
-            this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam.
-          </div>
-        </div>
-  
-        <div className="notifications">
-          <div className="profile">
-            
-              <img src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg" alt=""/>
-            
-          </div>
-          <div className="notification_value">
-            this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam.
-          </div>
-        </div>
-  
+
         <div className="dateline">
           <span>From</span>
           <p> 18/05/2021</p>
         </div>
-  
+
+
+        {NotificationData.map((item) => {
+          return (
+            <div className="notifications">
+              <div className="profile">
+                <img
+                  src={item.profileimg}
+                  alt=""
+                />
+              </div>
+              <div className="notification_value">
+               {item.notification}
+              </div>
+            </div>
+          );
+        })}
         <div className="notifications">
           <div className="profile">
-            
-              <img src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg" alt=""/>
-            
+            <img
+              src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg"
+              alt=""
+            />
           </div>
           <div className="notification_value">
-            this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam.
+            this is notification Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.accusantium ipsam.
           </div>
         </div>
-  
+
         <div className="notifications">
           <div className="profile">
-            
-              <img src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg" alt=""/>
-            
+            <img
+              src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg"
+              alt=""
+            />
           </div>
           <div className="notification_value">
-            this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam.
+            this is notification Lorem ipsum dolor sit amet consectetur
+            adipisicing elit.accusantium ipsam.
           </div>
         </div>
-  
-        <div className="notifications">
-          <div className="profile">
-            
-              <img src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg" alt=""/>
-            
-          </div>
-          <div className="notification_value">
-            this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam.
-          </div>
-        </div>
-  
-        <div className="notifications">
-          <div className="profile">
-            
-              <img src="https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg" alt=""/>
-            
-          </div>
-          <div className="notification_value">
-            this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam.
-          </div>
-        </div>     
-       
+
+     
       </div>
-      </div>
-    );
-  };
+    </div>
+  );
+};
---
+++ src/pages/Home/components/Notification/NotificationData.js
@@ -0,0 +1,12 @@
+
+export const NotificationData=[
+    {
+        profileimg:"https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg",
+        notification:" this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam."
+    },
+    {
+        profileimg:"https://cdn.britannica.com/38/130638-050-DBCE19EE/Kate-Winslet-2009.jpg",
+        notification:" this is notification Lorem ipsum dolor sit amet consectetur adipisicing elit.accusantium  ipsam."
+    },
+   
+]
